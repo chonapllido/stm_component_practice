52e5cbabe858686b573d31501d1531e55df79665
cabacangwork <cabacangwork@gmail.com>
2019-06-03 16:21:30 +0800

    Updates

--- js/custom-tab.js
+++ js/custom-tab.js
@@ -10,4 +10,14 @@ $(function(){
         $(this).parents("li").siblings().removeClass("on");
         $(this).parents("li").addClass("on");
     });
+    $(".small-combi-tab li a").on("click", function (e) {
+        e.preventDefault();
+        $(this).parents("li").siblings().removeClass("on");
+        $(this).parents("li").addClass("on");
+    });
+    $(".small-tab li a").on("click", function (e) {
+        e.preventDefault();
+        $(this).parents("li").siblings().removeClass("on");
+        $(this).parents("li").addClass("on");
+    });
 });
