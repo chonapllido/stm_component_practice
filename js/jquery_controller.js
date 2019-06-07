46ffcdbcb9f0cfc8d31a862072180a54c4f90f09
cabacangwork <cabacangwork@gmail.com>
2019-06-07 13:26:41 +0800

    Update

--- js/jquery_controller.js
+++ js/jquery_controller.js
@@ -5,4 +5,10 @@ $(function(){
         $(this).parents("li").siblings().removeClass("on");
         $(this).parents("li").addClass("on");
     });
+
+    $(".button-tabs li a").on("click", function (e) {
+        e.preventDefault();
+        $(this).parents("li").siblings().removeClass("on");
+        $(this).parents("li").addClass("on");
+    });
 });
