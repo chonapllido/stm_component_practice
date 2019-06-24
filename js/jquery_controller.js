963cba659476d25c7b6a08e4831cb0640839c190
cabacangwork <cabacangwork@gmail.com>
2019-06-24 11:16:56 +0800

    Updates

--- js/jquery_controller.js
+++ js/jquery_controller.js
@@ -5,7 +5,7 @@ $(function(){
 		fnCalendar('input.calendar');
 	} catch (e){}
 	
-    $("ul.combi-normal-tab li a, ul.normal-tab li a, ul.combi-small-tab li a, ul.small-tab li a, ul.large-tab li a, ul.combi-large-tab li a ").on("click", function (e) {
+    $("tab-combi li a, .general-tab li a").on("click", function (e) {
         e.preventDefault();
         $(this).parents("li").siblings().removeClass("on");
         $(this).parents("li").addClass("on");
